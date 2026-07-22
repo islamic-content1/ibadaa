3d8a9826c78b2ffe85158472bdb3dc44e50ff3c7
islamic-content1 <s12216944@stu.najah.edu>
2026-07-22 22:17:11 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'ibada-three-apps-v1';
+const FILES = [
+  './',
+  './index.html',
+  './index2.html',
+  './index3.html',
+  './manifest1.json',
+  './manifest2.json',
+  './manifest3.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
